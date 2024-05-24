5bf3b25c5b95b5aa8a842efd68363068404c9b4e
Oliver Janik <oliver.janik@fau.de>
2024-05-24 13:35:38 +0200

    Completed base structure of pipeline

--- src/BESTIE/pipeline.py
+++ src/BESTIE/pipeline.py
@@ -1,37 +1,50 @@
 import jax.numpy as jnp
 from jax import hessian
+from .utilities import parse_yaml
 
 class Pipeline():
-    def __init__(config_path):
-        self.config = parse_config(config_path)
+    def __init__(self,config_path):
+        self.config = parse_yaml(config_path)
         self.pipeline = None
-    
-    def get_pipeline():
-        if self.pipeline == None:
+
+        self.data = None
+        self.aux = None
+        self.injected_params = None
+
+        self.calc_weights = None
+        self.net = None
+        self.calc_data_hist = None
+        self.calc_hist = None
+        self.calc_loss = None
+
+    def get_pipeline(self, rebuild = False):
+        if self.pipeline == None or rebuild:
             self.pipeline = self.set_pipeline()
 
         return self.pipeline
 
-    def set_pipeline():
-        def pipeline(params):
-	    lss = net.apply({"params": params},data)
-	    data_hist = calc_data_hist(injected_params,data,aux)
-
-	    fish = hessian(analysis_llh)(injected_params,lss,aux,data_hist)
-	    CovMat = jnp.linalg.solve(fish)
-
-	    loss = calc_loss(CovMat)
-
-	    return loss
-
+    def set_pipeline(self):
         def analysis_llh(injected_params,lss,aux,data_hist):
-	    weights = calc_weights(injected_params,aux)
-	    hist = calc_hist(lss,weights)
-	    llh = calc_llh(hist,data_hist)
+            weights = self.calc_weights(injected_params,aux)
+            hist = self.calc_hist(lss,weights)
+            llh = self.calc_llh(hist,data_hist)
 
-	    return llh
+            return llh
+        
+        def pipeline(params):
+            lss = self.net.apply({"params": params},self.data)
+            data_hist = self.calc_data_hist(self.injected_params,self.data,self.aux)
+
+            fish = hessian(analysis_llh)(self.injected_params,lss,self.aux,data_hist)
+            CovMat = jnp.linalg.solve(fish)
+
+            loss = self.calc_loss(CovMat)
+
+            return loss
 
         self.pipeline = pipeline
 
+if __name__ == "__main__":
+    print("This is a module meant for importing only, NOT a script that can be executed!")
 
 
