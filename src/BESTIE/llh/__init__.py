9adb68f27dd7fceb275a85a303840d10cfdd9014
Oliver Janik <oliver.janik@fau.de>
2024-05-26 10:15:08 +0200

    Added poisson llh

--- src/BESTIE/llh/__init__.py
+++ src/BESTIE/llh/__init__.py
@@ -1,1 +1,2 @@
 from .llh_handler import llh_handler
+from .poisson_llh import poisson_llh
---
+++ src/BESTIE/llh/poisson_llh.py
@@ -0,0 +1,10 @@
+import jax.numpy as jnp
+from jax.scipy.special import gammaln
+
+def poisson_llh(k, mu):
+    llh = jnp.where(mu>0,
+                    k * jnp.log(mu) - mu - gammaln(k + 1),
+                    jnp.where(k>0,
+                              -690*k,
+                              0))
+    return llh
