e648eff04e10a6eb8c8283d3c62ad133889ab57a
Oliver Janik <oliver.janik@fau.de>
2024-06-04 17:30:26 +0200

    Added config implementation

--- src/BESTIE/hists/hist_handler.py
+++ src/BESTIE/hists/hist_handler.py
@@ -1,11 +1,14 @@
 from functools import partial
+import jax.numpy as jnp
 
 def hist_handler(config):
     method = config["method"]
 
     if method.lower() in ["binned_kde","bkde","binnedkde"]:
         from .bKDE import bKDE
-        calc_hist = partial(bKDE, bandwidth=config["bandwidth"])
+        bandwidth = float(config["bandwidth"])
+        bins = jnp.linspace(int(config["bins_low"]),int(config["bins_up"]),int(config["bins_number"])+1)
+        calc_hist = partial(bKDE, bins=bins, bandwidth=bandwidth)
         
     elif method.lower() in ["softmax"]:
         raise NotImplementedError("Softmax binning is not yet implemented")
