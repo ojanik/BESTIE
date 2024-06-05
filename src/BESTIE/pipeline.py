2b7cc485cd415075d4e795c70d972e3512dcbad4
Oliver Janik <oliver.janik@fau.de>
2024-06-05 13:17:08 +0200

    Removed unused variables

--- src/BESTIE/pipeline.py
+++ src/BESTIE/pipeline.py
@@ -10,17 +10,8 @@ class AnalysisPipeline():
     def __init__(self,config_path):
         self.config = parse_yaml(config_path)
         self._analysis_pipeline = None
-
-        # self.data = None REMOVE?
-        # self.aux = None REMOVE? 
-        # self.injected_params = None REMOVE?
-
         self.calc_weights = weight_handler(self.config["weights"])
-        #model = model_handler(self.config) REMOVE?
-        #self.net = model()
-        #self.calc_data_hist = None REMOVE?
         self.calc_hist = hist_handler(self.config["hists"])
-        # self.calc_loss = None REMOVE?
         self.calc_llh = llh_handler(self.config["llh"])
 
     def get_analysis_pipeline(self, rebuild = False):
