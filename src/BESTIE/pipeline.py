5e1b73c6ebe020c71623e6a3f2680fc83163570b
Oliver Janik <oliver.janik@fau.de>
2024-06-05 16:11:53 +0200

    Added OptimizationPipeline

--- src/BESTIE/pipeline.py
+++ src/BESTIE/pipeline.py
@@ -30,7 +30,40 @@ class AnalysisPipeline():
 
             return llh
         self._analysis_pipeline = analysis_pipeline
- 
+
+
+from .nets import model_handler
+from .losses import loss_handler
+
+class Optimization_Pipeline(AnalysisPipeline):
+    def __init__(self,config_path):
+        super().__init__(config_path)
+
+        self._optimization_pipeline = None
+
+        self.model = model_handler(self.config["network"])
+        self.net = self.model()
+
+        self.calc_loss = loss_handler([self.config["loss"]])
+
+        self._set_analysis_pipeline()
+
+
+    def get_optimization_pipeline(self, rebuild = False):
+        if self._analysis_optimization == None or rebuild:
+            self._set_optimization_pipeline()
+
+        return self._optimization_pipeline
+
+    def _set_optimization_pipeline(self):
+        @jit
+        def optimization_pipeline(net_params,injected_params,data,aux,data_hist):
+            lss = self.net.apply({"params":net_params},data)[:,0]
+            loss = self.calc_loss(self._analysis_pipeline,injected_params,lss,aux,data_hist)
+
+            return loss
+        self._optimization_pipeline = optimization_pipeline
+
 if __name__ == "__main__":
     print("This is a module meant for importing only, NOT a script that can be executed!")
 
