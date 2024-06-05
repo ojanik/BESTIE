30fc170fd33100e57ba7614bd2c7f8a7b2fd1d49
Oliver Janik <oliver.janik@fau.de>
2024-06-05 14:13:09 +0200

    Made loss_handler more general

--- src/BESTIE/losses/loss_handler.py
+++ src/BESTIE/losses/loss_handler.py
@@ -1,12 +1,25 @@
 
+
+
 def loss_handler(config):
 
     loss_method = config["loss"]
+    if loss_method.lower() in ["fisher"]:
 
-    if loss_method.lower() == "a_optimality":
-        from .fisher_losses import a_optimality
-        loss = a_optimality
-    
+        optimality = config["optimality"]
+
+        if optimality.lower() in ["a","a_optimality","aoptimality"]
+            from .fisher_losses import a_optimality
+            opti = a_optimality
+
+        else:
+            raise NotImplementedError(f"The {optimality} method for optimality is not yet implemented")
+        
+        from jax import hessian
+        def loss(llh,injected_params,lss,aux,data_hist):
+            fish = hessian(llh)(injected_params,lss,aux,data_hist)
+            return opti(fish,signal_idx=[0,1]) #TODO implement signal_idx readout from config
+
     else:
         raise NotImplementedError("The selected loss method is not implemented")
     
