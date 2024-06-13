2faf11ee7dff942a915697e0cbb0c61bdc7fe468
Oliver Janik <oliver.janik@fau.de>
2024-06-13 11:26:55 +0200

    Added missing minus to fisher information

--- src/BESTIE/losses/fisher_losses.py
+++ src/BESTIE/losses/fisher_losses.py
@@ -1,9 +1,13 @@
 import jax.numpy as jnp
 
+def S_optimality(fisher,signal_idx=[0]):
+    cov = -1 * jnp.linalg.inv(fisher)
+    uncert = jnp.diag(cov)
+    return uncert[signal_idx[0]]
+
 def A_optimality(fisher,signal_idx=None):
-    cov = jnp.linalg.inv(fisher)
+    cov = -1 * jnp.linalg.inv(fisher)
     diag = jnp.diag(cov)
-    
 
     if signal_idx == None:
         trace = jnp.sum(diag)
