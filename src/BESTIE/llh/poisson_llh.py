d0874d089d3a0ab28d8dd8b476cb499c2bddbb87
Oliver Janik <oliver.janik@fau.de>
2024-06-13 11:26:33 +0200

    Added small term to mu

--- src/BESTIE/llh/poisson_llh.py
+++ src/BESTIE/llh/poisson_llh.py
@@ -1,7 +1,9 @@
 import jax.numpy as jnp
 from jax.scipy.special import gammaln
 
+
 def poisson_llh(k, mu):
+    mu = mu + 1e-12
     llh = jnp.where(mu>0,
                     k * jnp.log(mu) - mu - gammaln(k + 1),
                     jnp.where(k>0,
