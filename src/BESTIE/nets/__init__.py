6bf05fe04bb7e423bd4e26bcd27b6d2014041a1d
Oliver Janik <oliver.janik@fau.de>
2024-06-24 11:47:58 +0200

    Added learning_rate_handler

--- src/BESTIE/nets/__init__.py
+++ src/BESTIE/nets/__init__.py
@@ -1,3 +1,5 @@
 from .model_handler import model_handler
 from .build_jax import build_jax
 from .utilities import *
+from .lr_handler import lr_handler
+from .learning_rates import *
---
+++ src/BESTIE/nets/lr_handler.py
@@ -0,0 +1,19 @@
+
+
+def lr_handler(config,steps_per_epoch):
+    method = config["training"]["lr"]["method"]
+
+    if method.lower() in ["constant"]:
+        lr = config["training"]["lr"]["lr"]
+
+    elif method.lower() in ["cosine","cos"]:
+        from .learning_rates import create_cosine_lr
+        lr = create_cosine_lr(config["training"]["lr"]["lr"],
+                              steps_per_epoch=steps_per_epoch,
+                              num_epochs=config["training"]["epochs"],
+                              warmup_epochs=0)
+    
+    else:
+        raise NotImplementedError(f"lr method {method} is not implemented")
+    
+    return lr
