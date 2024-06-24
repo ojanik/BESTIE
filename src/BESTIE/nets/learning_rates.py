79c2b11d7d2a0644242fdf7c2a9233346e1feda7
Oliver Janik <oliver.janik@fau.de>
2024-06-24 11:48:26 +0200

    Added cyclic lr

--- src/BESTIE/nets/learning_rates.py
+++ src/BESTIE/nets/learning_rates.py
@@ -15,3 +15,11 @@ def create_cosine_lr(base_learning_rate, steps_per_epoch=1,num_epochs = 1000,war
       boundaries=[warmup_epochs * steps_per_epoch])
     return schedule_fn
 
+def create_cyclic_lr(max_lr,min_lr,number_of_epochs,number_of_periods):
+  if max_lr < min_lr:
+          raise ValueError("min_lr cannot be larger than max_lr")
+  def cyclic_schedule_fn(x):
+      diff = max_lr - min_lr
+      T = number_of_epochs / number_of_periods
+      return 10**((diff/2)*(jnp.cos(jnp.pi*(2*x/T))+1)+min_lr)
+  return cyclic_schedule_fn
