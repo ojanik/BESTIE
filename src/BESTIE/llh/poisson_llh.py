f7722f8494786a3573ff28bba596081898825794
Oliver Janik <oliver.janik@fau.de>
2024-07-17 13:06:23 +0200

    Fixed problem with nans and high losses

--- src/BESTIE/llh/poisson_llh.py
+++ src/BESTIE/llh/poisson_llh.py
@@ -3,7 +3,8 @@ from jax.scipy.special import gammaln
 
 
 def poisson_llh(k, mu):
-    mu = mu + 1e-12
+    mu = mu + 1e-8
+    k = k + 1e-8
     llh = jnp.where(mu>0,
                     k * jnp.log(mu) - mu - gammaln(k + 1),
                     jnp.where(k>0,
