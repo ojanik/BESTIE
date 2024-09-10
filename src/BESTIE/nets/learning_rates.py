db77675caf094b9525872eab6fa012c733fdc027
Oliver Janik <oliver.janik@fau.de>
2024-09-10 14:05:51 +0200

    Added cosine learning rate with resets

--- src/BESTIE/nets/learning_rates.py
+++ src/BESTIE/nets/learning_rates.py
@@ -15,6 +15,33 @@ def create_cosine_lr(base_learning_rate, steps_per_epoch=1,num_epochs = 1000,war
       boundaries=[warmup_epochs * steps_per_epoch])
     return schedule_fn
 
+def create_cosine_reset_lr(base_learning_rate,steps_per_epoch=1,num_epochs=1000,warmup_epochs=10, number_of_resets = 3):
+    warmup_fn = optax.linear_schedule(
+      init_value=0., end_value=base_learning_rate,
+      transition_steps=warmup_epochs * steps_per_epoch)
+     
+    cosine_epochs = max((num_epochs - warmup_epochs)/number_of_resets, 1)
+
+    schedule_fns = [warmup_fn]
+    boundaries = [warmup_epochs * steps_per_epoch]
+    for j in range(number_of_resets):
+        cosine_fn = optax.cosine_decay_schedule(
+            init_value=base_learning_rate,
+            decay_steps=cosine_epochs * steps_per_epoch
+        )
+        schedule_fns.append(cosine_fn)
+        boundaries.append((warmup_epochs+(j+1)*cosine_epochs)*steps_per_epoch)
+
+    schedule_fn = optax.join_schedules(
+        schedules = schedule_fns,
+        boundaries=boundaries
+    )
+
+    return schedule_fn
+          
+
+     
+
 def create_cyclic_lr(max_lr,min_lr,number_of_epochs,number_of_periods):
   if max_lr < min_lr:
           raise ValueError("min_lr cannot be larger than max_lr")
--- src/BESTIE/nets/lr_handler.py
+++ src/BESTIE/nets/lr_handler.py
@@ -6,6 +6,8 @@ def lr_handler(config,steps_per_epoch):
     if method.lower() in ["constant","scalar"]:
         lr = config["training"]["lr"]["lr"]
 
+    
+
     elif method.lower() in ["cosine","cos"]:
         from .learning_rates import create_cosine_lr
         lr = create_cosine_lr(config["training"]["lr"]["lr"],
@@ -13,6 +15,14 @@ def lr_handler(config,steps_per_epoch):
                               num_epochs=config["training"]["epochs"],
                               warmup_epochs=0)
     
+    elif method.lower() in ["cosine_reset","cos_reset"]:
+        from .learning_rates import create_cosine_reset_lr
+        lr = create_cosine_reset_lr(config["training"]["lr"]["lr"],
+                                    steps_per_epoch=steps_per_epoch,
+                                    num_epochs=config["training"]["epochs"],
+                                    warmup_epochs=0,
+                                    number_of_resets=config["training"]["resets"])
+
     else:
         raise NotImplementedError(f"lr method {method} is not implemented")
     
