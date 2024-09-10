6a150876fd9e9e261395922bc03f6a4bd0431694
Oliver Janik <oliver.janik@fau.de>
2024-09-10 15:07:24 +0200

    Added transformations

--- src/BESTIE/__init__.py
+++ src/BESTIE/__init__.py
@@ -2,6 +2,7 @@ from . import hists
 from . import nets
 from . import utilities
 from . import llh
+from . import transformations
 from .pipeline import *
 #from .training import *
 from . import data
