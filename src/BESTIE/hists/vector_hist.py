0b6813d6f1ad97d1c94a4ce37d4b3f12611963ab
Oliver Janik <oliver.janik@fau.de>
2024-09-10 15:05:29 +0200

    Made vector hist compatible with output of lss calculation

--- src/BESTIE/hists/vector_hist.py
+++ src/BESTIE/hists/vector_hist.py
@@ -1,7 +1,12 @@
 import jax.numpy as jnp
 
-def vector_hist(lss,weights):
+def vector_hist_old(lss,weights):
     lss = lss *weights
     counts = jnp.sum(lss,axis=0)
 
     return counts
+
+def vector_hist(lss,weights):
+    counts = jnp.dot(jnp.transpose(weights),lss)
+    
+    return jnp.squeeze(counts)
