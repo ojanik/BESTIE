823d92d37b9d873e817d9d850e7ebf9897b2ff1b
Oliver Janik <oliver.janik@fau.de>
2024-09-13 12:02:45 +0200

    Moved plot_routine to utilities

--- plot_results.py
+++ plot_results.py
@@ -12,14 +12,17 @@ import os
 import argparse
 
 
-def main(model_path,
+def plot_routine_dep(model_path,
          save_to=None,
          make_gif=False,
          make_weighted_hist=False,
          make_unweighted_hist=False,
          make_2D_scatter=False,
+         make_2D_scatter_galactic=False,
          make_trainstep_loss_curve=False,
-         all_flag=False):
+         all_flag=False,
+         galactic=False,
+         save_df=False):
 
     path_to_config = os.path.join(model_path,"config.yaml")
     config = BESTIE.utilities.parse_yaml(path_to_config)
@@ -51,25 +54,60 @@ def main(model_path,
     results_dict = jnp.load(os.path.join(model_path,"result.pickle.npy"),allow_pickle=True)
     params = results_dict.item()["params"]
 
+    B = results_dict.item()["ffm"]["B"]
+
     batch_size = 10000
     num_parts = int(jnp.ceil(len(input_data)/batch_size))
     apply_fn = jit(net.apply)
     print("--- Calculating lss ---")
     for i in tqdm(range(num_parts),disable=True):
+        batched_input_data = input_data[i*batch_size:jnp.min(Array([(i+1)*batch_size,len(input_data)])),:len(config["dataset"]["input_vars"])]
+        batched_input_data = BESTIE.data.fourier_feature_mapping.input_mapping(batched_input_data,B)
         if i == 0:
-            lss = apply_fn({"params": params},input_data[i*batch_size:jnp.min(Array([(i+1)*batch_size,len(input_data)])),:len(config["dataset"]["input_vars"])])[:,0]
+            lss = apply_fn({"params": params},batched_input_data)[:,0]
         else:
-            lss = jnp.concatenate([lss,apply_fn({"params": params},input_data[i*batch_size:jnp.min(Array([(i+1)*batch_size,len(input_data)])),:len(config["dataset"]["input_vars"])])[:,0]])
-    del(input_data)
+            lss = jnp.concatenate([lss,apply_fn({"params": params},batched_input_data)[:,0]])
+    
 
     #shift lss to be between 0 and 1 like it is done during training
+    # transform function
+
+    
+    
     mask = mask_exists&mask_cut
-    mask2 = ~jnp.isnan(lss[mask])
-    lss -= jnp.min(lss[mask][mask2])
-    lss /= jnp.max(lss[mask][mask2])
+
+    lss = lss[mask]
+
+    kwargs = {}
+    kwargs["lss0"] = Array(df["lss0_standard_binning"])[mask]
+    
+    #phi0
+
+    init_params = results_dict.item()["init_params"]
+    for i in tqdm(range(num_parts),disable=True):
+        batched_input_data = input_data[i*batch_size:jnp.min(Array([(i+1)*batch_size,len(input_data)])),:len(config["dataset"]["input_vars"])]
+        batched_input_data = BESTIE.data.fourier_feature_mapping.input_mapping(batched_input_data,B)
+        if i == 0:
+            phi0 = apply_fn({"params": init_params},batched_input_data)[:,0]
+        else:
+            phi0 = jnp.concatenate([phi0,apply_fn({"params": init_params},batched_input_data)[:,0]])
+    del(input_data)
+
+    kwargs["phi0"] = phi0[mask]
+
+    transform_fun = BESTIE.transformations.transformation_handler(config["transformation"])
+
+    lss = transform_fun(lss,**kwargs)
+
+    #mask2 = ~jnp.isnan(lss[mask])
+    #lss -= jnp.min(lss[mask][mask2])
+    #lss /= jnp.max(lss[mask][mask2])
 
     injected_params = config["injected_params"]
     obj = BESTIE.Optimization_Pipeline(config,list(injected_params.keys()))
+
+    print("lss: ",lss)
+    print("lss_0: ",kwargs["lss0"])
 
     if make_trainstep_loss_curve or all_flag:
         print("--- Making trainstep loss curve ---")
@@ -86,18 +124,26 @@ def main(model_path,
     if make_weighted_hist or all_flag:
         print("--- Making weighted hist ---")
         i_params = injected_params.copy()
-        hvar = lss[mask]
+        hvar = lss#[mask]
         bins = onp.linspace(config["hists"]["bins_low"],config["hists"]["bins_up"],config["hists"]["bins_number"])
         H_total,_,_,_ = plt.hist2d(hvar,hvar,bins=[bins,bins],weights = obj.calc_weights(i_params,aux)[:,0])
         i_params["conv_norm"] = 0
         i_params["prompt_norm"] = 0
+        if galactic:
+            i_params["galactic_norm"] = 0
         H_astro,_,_,_ = plt.hist2d(hvar,hvar,bins=[bins,bins],weights = obj.calc_weights(i_params,aux)[:,0])
         i_params["astro_norm"] = 0
         i_params["conv_norm"] = 1.
         H_conv,_,_,_ = plt.hist2d(hvar,hvar,bins=[bins,bins],weights = obj.calc_weights(i_params,aux)[:,0])
         i_params["prompt_norm"] = 1.
         i_params["conv_norm"] = 0
+
         H_prompt,_,_,_ = plt.hist2d(hvar,hvar,bins=[bins,bins],weights = obj.calc_weights(i_params,aux)[:,0])
+
+        if galactic:
+            i_params["prompt_norm"] = 0.
+            i_params["galactic_norm"] = 1.
+            H_gal,_,_,_ = plt.hist2d(hvar,hvar,bins=[bins,bins],weights = obj.calc_weights(i_params,aux)[:,0])
 
 
 
@@ -106,8 +152,11 @@ def main(model_path,
         plt.grid()
         #ax.stairs(H_total.sum(axis=1),jnp.linspace(-1,1,nob+1),label="total")
         ax.stairs(H_astro.sum(axis=1),bins,label="astro")
+        print(H_astro.sum(axis=1))
         ax.stairs(H_conv.sum(axis=1),bins,label="conv")
         ax.stairs(H_prompt.sum(axis=1),bins,label="prompt")
+        if galactic:
+            ax.stairs(H_gal.sum(axis=1),bins,label="galactic")
         ax.set_yscale("log")
         ax.set_xlabel("lss")
         ax.set_ylabel("events")
@@ -120,7 +169,7 @@ def main(model_path,
 
     if make_unweighted_hist or all_flag:
         print("--- Making unweighted hist ---")
-        hvar = lss[mask]
+        hvar = lss#[mask]
         bins = onp.linspace(config["hists"]["bins_low"],config["hists"]["bins_up"],config["hists"]["bins_number"])
         H,_,_,_ = plt.hist2d(hvar,hvar,bins=[bins,bins])
         plt.close()
@@ -140,16 +189,33 @@ def main(model_path,
         nob = config["hists"]["bins_number"]
         digi = jnp.digitize(lss,bins=jnp.linspace(0,1,nob+1))
 
-        plt.scatter(df["energy_truncated"],jnp.cos(Array(df["zenith_MPEFit"])),c=digi,cmap="tab20")
+        plt.scatter(Array(df["energy_truncated"])[mask],jnp.cos(Array(df["zenith_MPEFit"]))[mask],c=digi,cmap="tab20")
         plt.xscale("log")
         plt.xlabel("reco energy")
-        plt.ylabel("cos(zenith)")
+        plt.ylabel("cos(reco zenith)")
         plt.colorbar()
 
         plt.ylim(-1.05,0.05)
-        plt.xlim(1e-1,1e9)
+        plt.xlim(1e1,1e8)
 
         plt.savefig(os.path.join(model_path,"2D_energy_coszenith.png"),dpi=256)
+        plt.close()
+
+    if make_2D_scatter_galactic or all_flag:
+        print("--- Making 2D scatter ---")
+        nob = config["hists"]["bins_number"]
+        digi = jnp.digitize(lss,bins=jnp.linspace(0,1,nob+1))
+
+        plt.scatter(df["ra_MPEFit"],jnp.cos(Array(df["zenith_MPEFit"])),c=digi,cmap="tab20")
+        #plt.xscale("log")
+        plt.xlabel("reco ra")
+        plt.ylabel("cos(reco zenith)")
+        plt.colorbar()
+
+        plt.ylim(-1.05,0.05)
+        plt.xlim(-0.1,6.3)
+
+        plt.savefig(os.path.join(model_path,"2D_ra_coszenith.png"),dpi=256)
         plt.close()
 
     if make_gif or all_flag:
@@ -191,6 +257,22 @@ def main(model_path,
         df.to_hdf(save_to,"a")
         print(f"Saved dataframe with lss at {save_to}")
 
+    if save_df:
+
+        print("--- Saving dataframe with updated lss values ---")
+
+        assert len(df)==len(mask)
+        df["lss"] = -1.
+        df["lss"].mask(mask,lss,inplace=True)
+        df.to_parquet()
+        if ext[1:].lower() in ["parquet"]:
+            df.to_parquet(dataframe)
+        elif ext[1:].lower() in ["hdf","hd5"]:
+            df.to_hdf(dataframe)
+        
+        print(f"--- Saved dataframe at {dataframe} ---")
+
+    
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser(description="Process some paths and an optional name.")
@@ -200,6 +282,7 @@ if __name__=="__main__":
 
     parser.add_argument('--make_trainstep_loss_curve',action='store_true',help="Flag if loss curve for each training step should be created")
     parser.add_argument('--make_2D_scatter',action='store_true',help="Flag if 2D scatter plot with the colors corresponding to the bin should be created")
+    parser.add_argument('--make_2D_scatter_galactic',action='store_true',help="Flag if 2D scatter plot with the colors corresponding to the bin should be created")
     parser.add_argument('--make_weighted_hist',action='store_true',help="Flag if flux weighted hist should be created")
     parser.add_argument('--make_unweighted_hist',action='store_true',help="Flag if unweighted hist should be created")
 
@@ -211,15 +294,24 @@ if __name__=="__main__":
 
     parser.add_argument('--make_gif',action='store_true',help="Flag if gif of binning should be created")
 
+    parser.add_argument('--galactic',action='store_true',help="Flag if galactic component is included")
+
+    parser.add_argument('--save_df',action='store_true',help="Flag if df should be saved with the new lss values")
+
     args = parser.parse_args()
 
-    main(model_path=args.model_path,
+    from BESTIE.utilities import plot_routine
+
+    plot_routine(model_path=args.model_path,
          save_to=args.save_to,
          make_gif=args.make_gif,
          make_weighted_hist=args.make_weighted_hist,
          make_unweighted_hist=args.make_unweighted_hist,
          make_2D_scatter=args.make_2D_scatter,
+         make_2D_scatter_galactic=args.make_2D_scatter_galactic,
          make_trainstep_loss_curve=args.make_trainstep_loss_curve,
-         all_flag = args.all)
+         all_flag = args.all,
+         galactic = args.galactic,
+         save_df=args.save_df)
     
     print("DONE")
--- src/BESTIE/utilities/__init__.py
+++ src/BESTIE/utilities/__init__.py
@@ -1,2 +1,3 @@
 from .configs import *
 from .jax_utils import *
+from .plot_routine import plot_routine
