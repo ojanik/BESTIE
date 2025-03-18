26a5bf3475d60178d8e8fb7939b7fe6ec298b623
Oliver Janik <oliver.janik@fau.de>
2025-03-18 13:24:59 +0100

    Allow for multiple losses

--- BESTIE/losses/fisher_losses.py
+++ BESTIE/losses/fisher_losses.py
@@ -1,18 +1,82 @@
 import jax.numpy as jnp
 
+from jax import grad, jacfwd, vmap
+import jax
+import jax.numpy as jnp 
+from BESTIE.utilities import rearrange_matrix
+
+
+def loss_fisher_jac(llh,injected_params,lss,aux,data_hist,sample_weights,**kwargs):
+
+    signal_idx = kwargs.pop("signal_idx")
+    opti = kwargs.pop("opti")
+    weight_norm = kwargs.pop("weight_norm",None)
+
+    grads, sigma = jacfwd(llh,has_aux=True)(injected_params,lss,aux,data_hist,sample_weights,skip_llh=True,**kwargs)
+    mu, sigma = llh(injected_params,lss,aux,data_hist,sample_weights,skip_llh=True,**kwargs)
+    fish_i = vmap(lambda g: jnp.outer(g, g))(grads)
+    hist_counts = mu + 1e-8#llh(injected_params,lss,aux,data_hist,sample_weights,skip_llh=True,**kwargs) + 1e-8
+    nonzero_hist_counts = hist_counts != 0
+    fish_i = jnp.where(nonzero_hist_counts[:, None, None],
+                        fish_i / hist_counts[:, None, None], 
+                        jnp.zeros(fish_i.shape))  # Shape: (1650, 11, 11)
+    fish = jnp.sum(fish_i, axis=0) 
+
+    # marginalize fisher information
+    fish = rearrange_matrix(fish, signal_idx)
+    k = len(signal_idx)
+    A = fish[:k, :k]
+    B = fish[:k, k:]
+    C = fish[k:, k:]
+
+    # Compute the inverse of C
+    C_inv = jnp.linalg.inv(C)
+    
+    # Compute the Schur complement S = A - B * C_inv * B^T
+    S = A - B @ C_inv @ B.T
+
+    return opti(S,weight_norm)
+
+
+def loss_fisher(llh,injected_params,lss,aux,data_hist,sample_weights,**kwargs):
+    #grads = grad(llh)(injected_params,lss,aux,data_hist,sample_weights,**kwargs)
+    #fish = -1 * jnp.outer(grads,grads)
+    #jax.debug.print("fish grads: {fish}",fish=fish)
+    fish = hessian(llh)(injected_params,lss,aux,data_hist,sample_weights,**kwargs)
+
+    # marginalize fisher information
+    fish = rearrange_matrix(fish, lconfig["signal_idx"])
+    k = len(lconfig["signal_idx"])
+    A = fish[:k, :k]
+    B = fish[:k, k:]
+    C = fish[k:, k:]
+
+    # Compute the inverse of C
+    C_inv = onp.linalg.inv(C)
+
+    # Compute the Schur complement S = A - B * C_inv * B^T
+    S = A - B @ C_inv @ B.T
+
+    return opti(S)
+
 def calc_conv(fisher):
-    return jnp.linalg.inv(fisher) *-1.
+    return jnp.linalg.inv(fisher) #*-1.
 
 def S_optimality(fisher,signal_idx=[0]):
     cov = calc_conv(fisher)
     uncert = jnp.diag(cov)
     return uncert
 
-def A_optimality(fisher,signal_idx=None):
+def A_optimality(fisher,weight_norm=None):
     cov = calc_conv(fisher)
     diag = jnp.diag(cov)
 
-    trace = jnp.sum(jnp.sqrt(diag))
+    if weight_norm is not None:
+        if isinstance(weight_norm,list):
+            weight_norm = jnp.array(weight_norm)
+        trace = jnp.sum(jnp.sqrt(diag)/weight_norm)
+    else:
+        trace = jnp.sum(jnp.sqrt(diag))
 
     """else:
         trace = 0
--- BESTIE/losses/loss_handler.py
+++ BESTIE/losses/loss_handler.py
@@ -1,13 +1,15 @@
 
-
+import jax.numpy as jnp
+Array = jnp.array
 
 def loss_handler(config):
+    lconfig = config["loss"]
+    loss_method = lconfig["method"]
+    losses = []
+    loss_kwargs = {}
+    if any(elem.lower() in ["fisher"] for elem in loss_method):
 
-    loss_method = config["method"]
-
-    if loss_method.lower() in ["fisher"]:
-
-        optimality = config["optimality"]
+        optimality = lconfig["optimality"]
 
         if optimality.lower() in ["a","a_optimality","aoptimality"]:
             from .fisher_losses import A_optimality
@@ -24,65 +26,18 @@ def loss_handler(config):
         else:
             raise NotImplementedError(f"The {optimality} method for optimality is not yet implemented")
         
-        
+        loss_kwargs["opti"] = opti
+        loss_kwargs["signal_idx"] = lconfig["signal_idx"]
+        loss_kwargs["weight_norm"] = lconfig.get("weight_norm",None)
     
-        if config["fisher_method"].lower() in ["jacobian","expectation"]:
-            from jax import grad, jacfwd, vmap
-            import jax
-            import jax.numpy as jnp 
-            from BESTIE.utilities import rearrange_matrix
-            def loss(llh,injected_params,lss,aux,data_hist,sample_weights,**kwargs):
-                grads = jacfwd(llh)(injected_params,lss,aux,data_hist,sample_weights,skip_llh=True,**kwargs)
-                fish_i = vmap(lambda g: jnp.outer(g, g))(grads)
-                hist_counts = llh(injected_params,lss,aux,data_hist,sample_weights,skip_llh=True,**kwargs) + 1e-8
-                nonzero_hist_counts = hist_counts != 0
-                fish_i = jnp.where(nonzero_hist_counts[:, None, None],
-                                    fish_i / hist_counts[:, None, None], 
-                                    jnp.zeros(fish_i.shape))  # Shape: (1650, 11, 11)
-                fish = jnp.sum(fish_i, axis=0) 
-
-                # marginalize fisher information
-                fish = rearrange_matrix(fish, config["signal_idx"])
-                k = len(config["signal_idx"])
-                A = fish[:k, :k]
-                B = fish[:k, k:]
-                C = fish[k:, k:]
-
-                # Compute the inverse of C
-                C_inv = jnp.linalg.inv(C)
-                
-                # Compute the Schur complement S = A - B * C_inv * B^T
-                S = A - B @ C_inv @ B.T
-                return opti(S,signal_idx=config["signal_idx"])
-        
+        if lconfig["fisher_method"].lower() in ["jacobian","expectation"]:
+            from .fisher_losses import loss_fisher_jac
+            losses.append(loss_fisher_jac)
         else:
-            from jax import hessian, grad, jacfwd, vmap
-            import jax.numpy as jnp
-            import jax
-            import numpy as onp
-            from BESTIE.utilities import rearrange_matrix
-            def loss(llh,injected_params,lss,aux,data_hist,sample_weights,**kwargs):
-                #grads = grad(llh)(injected_params,lss,aux,data_hist,sample_weights,**kwargs)
-                #fish = -1 * jnp.outer(grads,grads)
-                #jax.debug.print("fish grads: {fish}",fish=fish)
-                fish = hessian(llh)(injected_params,lss,aux,data_hist,sample_weights,**kwargs)
+            from .fisher_losses import loss_fisher
+            losses.append(loss_fisher)
 
-                # marginalize fisher information
-                fish = rearrange_matrix(fish, config["signal_idx"])
-                k = len(config["signal_idx"])
-                A = fish[:k, :k]
-                B = fish[:k, k:]
-                C = fish[k:, k:]
-
-                # Compute the inverse of C
-                C_inv = onp.linalg.inv(C)
-
-                # Compute the Schur complement S = A - B * C_inv * B^T
-                S = A - B @ C_inv @ B.T
-
-                return opti(S)
-
-    elif loss_method.lower() in ["scan"]:
+    if False:
         raise NotImplementedError("Scan loss is currently not fully implemented")
         from .scan_loss import calc_scan_loss
 
@@ -94,9 +49,12 @@ def loss_handler(config):
         losses.append(bin_loss)
         loss_kwargs["df_length"] = config["dataset"]["length"]
 
-
+    if len(losses)==0:
+        raise NotImplementedError("No valid loss method selected")
+    
     else:
-        raise NotImplementedError("The selected loss method is not implemented")
+        def loss(llh,injected_params,lss,aux,data_hist,sample_weights,**kwargs):
+            return Array([l(llh,injected_params,lss,aux,data_hist,sample_weights,**kwargs,**loss_kwargs) for l in losses])
     
     return loss
 
