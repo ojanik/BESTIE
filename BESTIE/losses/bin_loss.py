ca7bc086c19359f0631435ef05b7da861c20a129
Oliver Janik <oliver.janik@fau.de>
2025-03-26 10:35:34 +0100

    Added bin loss

--- BESTIE/losses/bin_loss.py
+++ BESTIE/losses/bin_loss.py
@@ -2,8 +2,10 @@ import jax.numpy as jnp
 
 def bin_loss(llh,injected_params,lss,aux,data_hist,sample_weights,**kwargs):
     mu, sigma = llh(injected_params,lss,aux,data_hist,sample_weights,skip_llh=True,**kwargs)
-    df_length = kwargs.pop("df_length")
+    ds_length = kwargs.pop("df_length")
 
-    x = jnp.sqrt(sigma+1e-8)/(mu+1) / (jnp.sqrt(df_length/jnp.sum(mu>0)))
-
-    return jnp.sum(jnp.exp(abs(x))-1) 
+    # Calculate sqrt(ssq)/mu ratio for each bin
+    x = jnp.sqrt(sigma+1e-8)/(mu+1e-8) 
+    # Calculate average loss over all populated bins
+    loss = jnp.sum(x**2) / (jnp.sqrt(ds_length/jnp.sum(mu>0)))
+    return loss
