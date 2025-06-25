fd8a9f4d3b1b179232cf5550d9d0b8209efd5a7c
Oliver Janik <oliver.janik@fau.de>
2025-06-25 16:11:50 +0200

    Removed deprecated binnings

--- BESTIE/hists/__init__.py
+++ BESTIE/hists/__init__.py
@@ -1,5 +1,3 @@
 from .bKDE import *
 from .hist_handler import hist_handler
-from .standard_binning import standard_binning
-from .vector_hist import vector_hist
 from .tanh_binning import tanhHist, tanhHistND
