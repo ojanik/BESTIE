2262d510f246336cd16c4c16e3a646f5259076c8
Oliver Janik <oliver.janik@fau.de>
2025-07-01 16:35:14 +0200

    Updated __init__

--- BESTIE/__init__.py
+++ BESTIE/__init__.py
@@ -1,9 +1,9 @@
 from . import hists
 from . import nets
 from . import utilities
-from . import llh
 from . import transformations
 
 from . import data
 
+from .pipeline import Pipeline
 
--- BESTIE/transformations/__init__.py
+++ BESTIE/transformations/__init__.py
@@ -1,4 +1,3 @@
 from .transformation_handler import transformation_handler
 from .no_scaling import *
-from .cyclic_restricted_norm import *
 from .linear_scaling import *
